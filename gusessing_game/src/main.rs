15390135fd6f931437cadf6393e795c4c53458b2
tzh <zhihuatean@gmail.com>
2024-01-12 06:01:21 +0000

    1.add loop 2.add win breake

--- gusessing_game/src/main.rs
+++ gusessing_game/src/main.rs
@@ -8,22 +8,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
     println!("The secret number is {secret_number}");
-    
-    println!("please input your guess");
 
-    let mut guess = String::new();
+    loop {
+        println!("please input your guess");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let mut guess = String::new();
 
-    println!("your guessed:{guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("your guessed:{guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
